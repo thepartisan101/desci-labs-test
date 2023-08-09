5852952b5b7262bc6622000e56cb950c30219884
thepartisan101 <rseoane8@gmail.com>
2023-08-09 15:48:28 +0300

    update api call to add data to ipfs node

--- pages/api/ipfs.ts
+++ pages/api/ipfs.ts
@@ -9,29 +9,53 @@ export type BasicIpfsData = {
   content: string;
 };
 
+export type ErrorResponse = {
+  error: string;
+};
+
+
+// In-memory storage for notes
+let notes: BasicIpfsData[] = [];
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<BasicIpfsData>
+  res: NextApiResponse<BasicIpfsData | BasicIpfsData[] | ErrorResponse>
 ) {
   if (req.method === "POST") {
-    // Process a POST request
+    // Assuming you have a local IPFS API running on http://localhost:5001
+    const { text } = req.body;
+
+    if (!text) {
+      return res.status(400).json({ error: "Content is required." });
+    }
+
+    // Connect to the default IPFS API address http://localhost:5001
+    const client = create();
+    // Call Core API methods to add the content to IPFS
+    const data = await client.add(text);
+
+    console.log(data);
+
+    // Store the note in memory
+    const newNote: BasicIpfsData = { cid: data.path, content: text };
+    notes.push(newNote);
+
+    // Respond with the CID and content in the response
+    res.status(200).json(newNote);
+  } else if (req.method === "GET") {
+    // Handle GET request for retrieving data
+    retrieveData(req, res);
   } else {
     // Handle any other HTTP method
-    retrieveData(req, res);
+    res.status(405).json({ error: "Method not allowed." });
   }
 }
 
 const retrieveData = async (
   req: NextApiRequest,
-  res: NextApiResponse<BasicIpfsData>
+  res: NextApiResponse<BasicIpfsData[]>
 ) => {
-  // connect to the default API address http://localhost:5001
-  const client = create();
-  // call Core API methods
-  const string = "Hello world!";
-  const data = await client.add(string);
-
-  console.log(data);
-
-  res.status(200).json({ cid: data.path, content: string });
+  // Return the notes from the in-memory storage
+  res.status(200).json(notes);
 };
+
