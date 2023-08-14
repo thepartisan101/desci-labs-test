d07278ec351a06c63ecac7d6c55bc3f932e69fba
thepartisan101 <rseoane8@gmail.com>
2023-08-14 12:26:39 +0300

    add note loading on start, refactor components

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Layout from './components/layout'
 import { Inter } from "@next/font/google";
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import { BasicIpfsData } from "./api/ipfs";
 import { Button } from "./components/ui/button";
@@ -13,21 +13,51 @@ import { useAccount } from 'wagmi'
 
 const inter = Inter({ subsets: ["latin"] });
 
+// Type definitions
+type RefreshButtonProps = {
+  handleLoad: () => void;
+  loading: boolean;
+};
+
+type NoteProps = {
+  note: BasicIpfsData;
+};
+
+type NotesListProps = {
+  result: BasicIpfsData[] | null;
+  handleLoad: () => void;
+};
+
 export default function Home() {
-  const { address, isConnecting, isDisconnected } = useAccount()
+  const { address } = useAccount()
 
   const [loading, setLoading] = useState(false);
   const [result, setResult] = useState<BasicIpfsData[] | null>(null);
 
-  const [note, setNote] = useState<BasicIpfsData | null>(null);
-  const [txt, setTxt] = useState("");
 
-  const handleLoad = async () => {
+  const handleLoad = useCallback(async () => {
     setLoading(true);
-    const { data } = await axios.get<BasicIpfsData[]>("/api/ipfs");
-    setResult(data);
-    setLoading(false);
-};
+    axios.get<BasicIpfsData[]>(`/api/ipfs?address=${address}`)
+      .then(response => {
+        const data = response.data;
+        setResult(data);
+        console.log(data);
+        console.log("Address used for note retrieval:", address);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error("Error fetching data from /api/ipfs:", error);
+        setLoading(false);
+      });
+  }, [address]);
+  
+
+  useEffect(() => {
+    console.log("Current address:", address);
+    if (address !== null && address !== undefined) {
+      handleLoad();
+    }
+  }, [address, handleLoad]);  
 
 
   // avoiding ternary operators for classes
@@ -35,6 +65,43 @@ export default function Home() {
     return classes.filter(Boolean).join(" ");
   }
 
+
+  const RefreshButton: React.FC<RefreshButtonProps> = ({ handleLoad, loading }) => (
+    <Button
+      onClick={handleLoad}
+      size="icon"
+      variant="outline"
+      className={classNames(
+        "hover:bg-white text-white hover:text-black rounded-md p-2 drop-shadow-md w-32",
+        loading ? "animate-pulse" : ""
+      )}
+    >
+      <RefreshCw className="mr-2 h-4 w-4" />
+      {loading ? "Loading..." : "Show Notes"}
+    </Button>
+  );
+
+  const Note: React.FC<NoteProps> = ({ note }) => (
+    <div className="flex flex-col hover:bg-slate-800 rounded-sm p-3">
+      <span>{note.content}</span>
+      <span className="text-sm text-muted-foreground">
+        CID: {note.cid ? note.cid.substring(0, 30) + "..." : "N/A"}
+      </span>
+    </div>
+  );
+
+  const NotesList: React.FC<NotesListProps> = ({ result, handleLoad }) => (
+    <div className="w-2/3 flex flex-col gap-2 max-h-[50vh] overflow-y-auto">
+      {result && <RefreshButton handleLoad={handleLoad} loading={loading} />}
+      {result?.length ? (
+        result.map((note, i) => <Note key={i} note={note} />)
+      ) : (
+        <span>No notes found</span>
+      )}
+    </div>
+  );
+
+  
   return (
     <>
       <Head>
@@ -48,39 +115,14 @@ export default function Home() {
           <h1 className="scroll-m-20 text-xl font-semibold tracking-tight lg:text-3xl">
             Your IPFS Notes
           </h1>
-          {address ? 
+          {address ? (
             <div className="flex flex-col sm:flex-row gap-2">
               <NoteForm handleLoad={handleLoad} />
-              <div className="w-2/3 flex flex-col gap-2">
-                {result ? 
-                  <Button
-                    onClick={handleLoad}
-                    size="icon"
-                    variant="outline"
-                    className={classNames(
-                      "hover:bg-white text-white hover:text-black rounded-md p-2 drop-shadow-md w-32",
-                      loading ? "animate-pulse" : ""
-                      )}
-                      >
-                    <RefreshCw className="mr-2 h-4 w-4" />
-                    {loading ? "Loading..." : "Show Notes"}
-                  </Button>
-                  : null}
-                {result?.length ? (
-                  result.map((note, i) => (
-                    <div key={i} className="flex flex-col hover:border-2 hover:drop-shadow-sm rounded-sm p-3">
-                      <span>{note.content}</span>
-                      <span className="text-sm text-muted-foreground">CID: {(note.cid).substring(0, 30) + "..."}</span>
-                    </div>
-                  ))
-                ) : (
-                  <span>No notes found</span>
-                )}
-              </div>
+              <NotesList result={result} handleLoad={handleLoad} />
             </div>
-            :
+          ) : (
             <h3>Please connect your wallet to access</h3>
-          }
+          )}
         </div>
       </Layout>
     </>
